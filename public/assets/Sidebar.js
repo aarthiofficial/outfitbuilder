9079710b54762d0c259a60b6c1b744cc99e340cb
aarthiofficial <aarthi.nbs@gmail.com>
2025-04-24 19:38:45 +0530

    Create Sidebar.js

---
+++ public/assets/Sidebar.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    id: 'tshirt',
+    label: 'T-Shirt',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTYiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5USDwvdGV4dD48L3N2Zz4='
+  },
+  {
+    id: 'pants',
+    label: 'Pants',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTYiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5QQTwvdGV4dD48L3N2Zz4='
+  },
+  {
+    id: 'jacket',
+    label: 'Jacket',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTIiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5KQTwvdGV4dD48L3N2Zz4='
+  },
+  {
+    id: 'dress',
+    label: 'Dress',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTEiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5EUjwvdGV4dD48L3N2Zz4='
+  },
+  {
+    id: 'cap',
+    label: 'Cap',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTYiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5DUDwvdGV4dD48L3N2Zz4='
+  },
+  {
+    id: 'shoes',
+    label: 'Shoes',
+    icon: 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZGRkZGRkIi8+PHRleHQgeD0iMTAiIHk9IjM2IiBmb250LXNpemU9IjE2IiBmaWxsPSIjNjY2Ij5TSDwvdGV4dD48L3N2Zz4='
+  }
+];
